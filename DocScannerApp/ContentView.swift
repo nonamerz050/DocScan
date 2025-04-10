7c5b2dc32196fb0103777f2f6417c963c1872279
nonamerz050 <a.g.kolesnik@yandex.ru>
2025-04-10 19:36:08 +0800

    no message

--- DocScannerApp/ContentView.swift
+++ DocScannerApp/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @AppStorage("showIntroView") private var showIntroView: Bool = true
+    
     var body: some View {
         Home()
             .sheet(isPresented: $showIntroView) {
